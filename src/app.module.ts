2e372f85a1eede7ff27f6d5bd21a2dec3cfb3f8e
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2026-05-24 14:59:03 +0100

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { ConfigModule } from '@nestjs/config';
 import { PostsModule } from './posts/posts.module';
 import * as joi from 'joi';
 import appConfig from './config/app.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 // root module -> use all the sub modules
@@ -15,16 +16,27 @@ import appConfig from './config/app.config';
 
 @Module({
   imports: [ 
-    
-    ConfigModule.forRoot({
-      isGlobal: true, // make configmodule globally available
-      // validationSchema : joi.object({
-      //   APP_NAME: joi.string().default('defaultApp'),
 
-      // })
+    TypeOrmModule.forRoot({
 
-      load : [appConfig],
+        type : 'postgress',
+        host : 'localhost',
+        port : 5432,
+        username : 'postgress',
+        password : 'root',
+        database : 'youtube-nestjs-project',
+        
     }),
+    
+    // ConfigModule.forRoot({
+    //   isGlobal: true, // make configmodule globally available
+    //   // validationSchema : joi.object({
+    //   //   APP_NAME: joi.string().default('defaultApp'),
+
+    //   // })
+
+    //   load : [appConfig],
+    // }),
     
     HelloModule, UserModule, PostsModule],
   controllers: [AppController],
