c7f6f83a2799ba7d0083529e354bf9ec84f1fac5
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2026-06-03 23:43:24 +0100

    update

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,130 +10,156 @@ import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-
   constructor(
     @InjectRepository(User)
-    private userRepository : Repository<User>,
-    private jwtService : JwtService
-  ){}
+    private userRepository: Repository<User>,
+    private jwtService: JwtService,
+  ) {}
 
-  async register (registerDto : RegisterDto){
+  async register(registerDto: RegisterDto) {
     const existingUser = await this.userRepository.findOne({
-      where: {email : registerDto.email}
-    })
+      where: { email: registerDto.email },
+    });
 
-    if(existingUser){
-      throw new ConflictException('Email already in use! Please try with a different email')
+    if (existingUser) {
+      throw new ConflictException(
+        'Email already in use! Please try with a different email',
+      );
     }
 
-    const hashedPassword = await this.hashPassword(registerDto.password)
+    const hashedPassword = await this.hashPassword(registerDto.password);
 
     const newlyCreatedUser = this.userRepository.create({
       email: registerDto.email,
       name: registerDto.name,
       password: hashedPassword,
-      role: UserRole.USER
-    })
+      role: UserRole.USER,
+    });
 
-    const savedUser = await this.userRepository.save(newlyCreatedUser)
+    const savedUser = await this.userRepository.save(newlyCreatedUser);
 
-    const {password, ...result} = savedUser;
+    const { password, ...result } = savedUser;
 
     return {
       user: result,
-      message: 'Registration successfull! Please login to continue'
+      message: 'Registration successfull! Please login to continue',
+    };
+  }
+
+  async createAdmin(registerDto: RegisterDto) {
+    const existingUser = await this.userRepository.findOne({
+      where: { email: registerDto.email },
+    });
+
+    if (existingUser) {
+      throw new ConflictException(
+        'Email already in use! Please try with a different email',
+      );
     }
+
+    const hashedPassword = await this.hashPassword(registerDto.password);
+
+    const newlyCreatedUser = this.userRepository.create({
+      email: registerDto.email,
+      name: registerDto.name,
+      password: hashedPassword,
+      role: UserRole.ADMIN,
+    });
+
+    const savedUser = await this.userRepository.save(newlyCreatedUser);
+
+    const { password, ...result } = savedUser;
+
+    return {
+      user: result,
+      message: 'Admin user created successfull! Please login to continue',
+    };
   }
 
+  async login(loginDto: LoginDto) {
+    const user = await this.userRepository.findOne({
+      where: { email: loginDto.email },
+    });
 
-  async createAdmin(registerDto : RegisterDto){
-       const existingUser = await this.userRepository.findOne({
-         where: { email: registerDto.email },
-       });
-
-       if (existingUser) {
-         throw new ConflictException(
-           'Email already in use! Please try with a different email',
-         );
-       }
-
-       const hashedPassword = await this.hashPassword(registerDto.password);
-
-        const newlyCreatedUser = this.userRepository.create({
-          email: registerDto.email,
-          name: registerDto.name,
-          password: hashedPassword,
-          role: UserRole.ADMIN,
-        });
-
-            const savedUser = await this.userRepository.save(newlyCreatedUser);
-
-            const { password, ...result } = savedUser;
-
-            return {
-              user: result,
-              message: 'Admin user created successfull! Please login to continue',
-            };
-  }
-
-  async login (loginDto : LoginDto){
-    const user =  await this.userRepository.findOne({
-      where : {email : LoginDto.email }
-    })
-
-    if(!user || !(await this.verifyPassword(loginDto.password, user.password))){
-      throw new UnauthorizedException('Invalid credentials or account not exists')
+    if (
+      !user ||
+      !(await this.verifyPassword(loginDto.password, user.password))
+    ) {
+      throw new UnauthorizedException(
+        'Invalid credentials or account not exists',
+      );
     }
 
     // generate the tokens
-    const tokens = this.generateTokens(user)
-    const {password, ...result} = user;
+    const tokens = this.generateTokens(user);
+    const { password, ...result } = user;
     return {
       user: result,
-      ...tokens
+      ...tokens,
+    };
+  }
+
+  async refreshToken(refreshToken: string) {
+    try {
+      const payload = this.jwtService.verify(refreshToken, {
+        secret: 'refresh_secret',
+      });
+
+      const user = await this.userRepository.findOne({
+        where: { id: payload.sub },
+      });
+
+      if (!user) {
+        throw new UnauthorizedException('Invalid token');
+      }
+
+      const accessToken = this.generateAccessToken(user);
+
+      return { accessToken };
+    } catch (e) {
+      throw new UnauthorizedException('Invalid token');
     }
   }
 
-  private async hashPassword(password: string): Promise<string>{
-    return bcrypt.hash(password, 10)
+  private async hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, 10);
   }
 
-  private async verifyPassword(plainPassword : string, hashedPassword: string) : Promise<boolean>{
-    return bcrypt.compare(plainPassword, hashedPassword)
+  private async verifyPassword(
+    plainPassword: string,
+    hashedPassword: string,
+  ): Promise<boolean> {
+    return bcrypt.compare(plainPassword, hashedPassword);
   }
 
-  private generateTokens(user: User){
-    return{
-      accessToken : this.generateAccessToken(user),
-      refreshToken : this.generateRefreshToken(user)
-    }
+  private generateTokens(user: User) {
+    return {
+      accessToken: this.generateAccessToken(user),
+      refreshToken: this.generateRefreshToken(user),
+    };
   }
 
-  private generateAccessToken(user: User) : string {
+  private generateAccessToken(user: User): string {
     //  -> email, sub (id), role -> vvvI -> RBAC -> user ? Admin
     const payload = {
       email: user.email,
       sub: user.id,
-      role: user.role
-    }
+      role: user.role,
+    };
     return this.jwtService.sign(payload, {
       secret: 'jwt_secret',
-      expiresIn: '15m'
-    })
+      expiresIn: '15m',
+    });
   }
 
-  private generateRefreshToken(user: User) : string {
+  private generateRefreshToken(user: User): string {
+    const payload = {
+      sub: user.id,
+    };
 
-     const payload = {
-      
-       sub: user.id,
-       
-     };
-
-     return this.jwtService.sign(payload, {
-       secret: 'refresh_secret',
-       expiresIn: '7d',
-     });
-
+    return this.jwtService.sign(payload, {
+      secret: 'refresh_secret',
+      expiresIn: '7d',
+    });
   }
 }
